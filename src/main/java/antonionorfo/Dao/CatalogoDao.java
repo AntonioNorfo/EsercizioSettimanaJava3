abad240e2240a2ab635e4d2bbc604cf8cd1dbb30
nome-git <168424276+AntonioNorfo@users.noreply.github.com>
2024-10-11 15:35:07 +0200

    Accertamenti sul delete e controllo DB

--- src/main/java/antonionorfo/Dao/CatalogoDao.java
+++ src/main/java/antonionorfo/Dao/CatalogoDao.java
@@ -1,6 +1,7 @@
 package antonionorfo.Dao;
 
 import antonionorfo.Entities.Catalogo;
+import antonionorfo.Exception.ElementoNonTrovatoException;
 import jakarta.persistence.EntityManager;
 
 import java.util.UUID;
@@ -32,9 +33,10 @@ public class CatalogoDao {
     public void deleteById(UUID id) {
         em.getTransaction().begin();
         Catalogo catalogo = em.find(Catalogo.class, id);
-        if (catalogo != null) {
-            em.remove(catalogo);
+        if (catalogo == null) {
+            throw new ElementoNonTrovatoException("Non abbiamo trovato questo elemento specifico : " + id);
         }
+        em.remove(catalogo);
         em.getTransaction().commit();
     }
 }
