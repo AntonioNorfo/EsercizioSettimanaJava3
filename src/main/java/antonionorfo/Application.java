3d5da19fbac9f4fe5a983e94b3bbab93198f0f0c
nome-git <168424276+AntonioNorfo@users.noreply.github.com>
2024-10-11 13:16:57 +0200

    Creazioni di tabelle e classi , con estensioni.

--- src/main/java/antonionorfo/Application.java
+++ src/main/java/antonionorfo/Application.java
@@ -1,9 +1,16 @@
 package antonionorfo;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("ProgettoJava3");
+
+        EntityManager em = emf.createEntityManager();
     }
 }
 //
